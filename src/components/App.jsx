f215a81616798907ae3ad31bd14cbac546483b42
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-10-13 11:21:34 +0200

    finish game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
 //imports dependencias, imagenes, de otros componentes, de estilos
+//import { render } from 'node-sass';
 import '../styles/index.scss';
-
-import background from '../assets/blackboard.jpg';
-
-import font from '../fonts/KgTenThousandReasons-R1ll.ttf';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   // funciones, variables, handles...
   const [numberOfErrors, setNumberOfErrors] = useState(0);
   const [lastLetter, setLastLetter] = useState('');
-  const [word, setWord] = useState('pepino');
+  const [word, setWord] = useState('');
   const [userLetters, setUserLetters] = useState([]);
-  
+
+  useEffect(() => {
+    fetch('https://dev.adalab.es/api/random/word')
+      .then((response) => response.json())
+      .then((data) => {
+        setWord (data.word)
+      });
+  }, []);
+
   const handleClick = (event) => {
     event.preventDefault();
     setNumberOfErrors(numberOfErrors + 1);
   };
-
 
   const handleLastLetter = (ev) => {
     console.log(ev.target.value);
@@ -27,31 +31,45 @@ function App() {
 
       setLastLetter(ev.target.value);
       console.log(lastLetter);
-      setUserLetters([...userLetters,ev.target.value]);
-      console.log(userLetters)
+      setUserLetters([...userLetters, ev.target.value]);
+      console.log(userLetters);
     }
   };
-  const renderSolutionLetters = (ev) =>{
+  const renderSolutionLetters = () => {
     const wordLetters = word.split('');
-    
-    return wordLetters.map ((letter, index) =>{
-      
+    return wordLetters.map((letter, index) => {
       const newLetter = userLetters.includes(letter);
-      
       return (
-        <li className='letter' key = {index}>{newLetter
-          ?letter
-          :''
-          }</li>);
-        }
+        <li className="letter" key={index}>
+          {newLetter ? letter : ''}
+        </li>
       );
-    };
-       
-       
-    
-    
- 
-  
+    });
+  };
+
+  /****4. Pintando las letras falladas*** */
+  const renderErrorLetters = () => {
+    // Filtrar las userLetters que no existen en la palabra
+    const errorLetters = userLetters.filter((letter) => !word.includes(letter));
+
+    // Recorrer las letras erróneas y retornar un <li> para cada una
+    return errorLetters.map((letter, index) => (
+      <li className="letter" key={index}>
+        {letter}
+      </li>
+    ));
+  };
+
+  const calculateErrors = () => {
+    const errorCount = userLetters.filter(
+      (letter) => !word.includes(letter)
+    ).length;
+    return errorCount;
+  };
+  const renderDummy = () => {
+    const errorCount = calculateErrors();
+    return `dummy error-${errorCount}`;
+  };
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
@@ -68,20 +86,12 @@ function App() {
           <section>
             <div className="solution">
               <h2 className="title">Solución:</h2>
-              
-              <ul className="letters">
-              {renderSolutionLetters ()}
-              </ul>
+
+              <ul className="letters">{renderSolutionLetters()}</ul>
             </div>
             <div className="error">
               <h2 className="title">Letras falladas:</h2>
-              <ul className="letters">
-                <li className="letter">f</li>
-                <li className="letter">q</li>
-                <li className="letter">h</li>
-                <li className="letter">p</li>
-                <li className="letter">x</li>
-              </ul>
+              <ul className="letters">{renderErrorLetters()}</ul>
               <button className="button" onClick={handleClick}>
                 Incrementar
               </button>
@@ -102,7 +112,7 @@ function App() {
               />
             </form>
           </section>
-          <section className={'dummy error-' + numberOfErrors}>
+          <section className={renderDummy()}>
             <span className="error-13 eye"></span>
             <span className="error-12 eye"></span>
             <span className="error-11 line"></span>
